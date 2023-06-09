5a60ecabf65dcff7a289ad683d581908edd174d9
asarmichil <asarmichil@gmail.com>
2023-06-09 05:00:27 -0700

    Added feedback modals to register and update password pages

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,49 +1,68 @@
 import { Component, OnInit } from '@angular/core';
-import { NgbAlert } from '@ng-bootstrap/ng-bootstrap';
+import { NgbAlert, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Router } from '@angular/router';
 import { FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
+import { AuthError } from '@supabase/supabase-js';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  requestErrorMessage = '';
+  regSuccess = false;
   credentials = this.fb.nonNullable.group({
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(6)]],
   });
 
-  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {}
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private modalService: NgbModal
+  ) {}
 
-get email(){
-  return this.credentials.controls.email;
-}
+  get email() {
+    return this.credentials.controls.email;
+  }
 
-get password(){
-  return this.credentials.controls.password;
-}
+  get password() {
+    return this.credentials.controls.password;
+  }
 
+  async createAccount(modal) {
+    // TODO: Add loading spinner
 
-async createAccount(){
-// TODO: Add loading spinner
+    const data = await this.authService.signUp(this.credentials.getRawValue());
 
-const data = await this.authService.signUp(this.credentials.getRawValue())
+    // TODO REMOVE THIS AND MAKE IT AN ERROR POPUP
+    if (data.error) {
+      console.log('REGISTRATION FAILED');
+      this.regSuccess = false;
 
-// TODO REMOVE THIS AND MAKE IT AN ERROR POPUP
-if(data.error){
-  console.log('REGISTRATION FAILED')
-}else{
-  //TODO ADD POP UP FOR SUCCESS
-  // Signup succcess confirm email! text
+      this.requestErrorHandler(data.error);
+    } else {
+      //TODO ADD POP UP FOR SUCCESS
+      // Signup succcess confirm email! text
+      this.regSuccess = true;
+      // navigate back to home
+      console.log('REGISTRATION SUCCESS');
+    }
+    this.modalService.open(modal);
 
-  // navigate back to home
-  console.log('REGISTRATION SUCCESS')
-  this.router.navigate(['/']);
+  }
 
-}
+  requestErrorHandler(error: AuthError) {
+    console.log(error.message);
+    this.requestErrorMessage = '';
 
-}
-
+    if (error.status == 429) {
+      this.requestErrorMessage = 'Too many requests! ';
+    }
+    this.regSuccess = false;
+    this.requestErrorMessage += error.message;
+  }
 
 }
--- src/app/pages/update-password/update-password.component.ts
+++ src/app/pages/update-password/update-password.component.ts
@@ -1,26 +1,47 @@
 import { Component } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { AuthError, AuthResponse } from '@supabase/supabase-js';
+
+
 @Component({
   selector: 'app-update-password',
   templateUrl: './update-password.component.html',
   styleUrls: ['./update-password.component.scss'],
 })
 export class UpdatePasswordComponent {
+  updateSuccess = false;
+  requestErrorMessage='';
   credentials = this.fb.nonNullable.group({
     password: ['', [Validators.required, Validators.minLength(6)]],
   });
 
-  constructor(private fb: FormBuilder, private authService: AuthService) {}
+  constructor(private fb: FormBuilder, private authService: AuthService, private modalService: NgbModal) {}
 
   // TODO:
-  async updatePassword() {
+  async updatePassword(modal) {
     console.log(this.credentials.getRawValue().password)
     const data = await this.authService.updateUser({password: this.credentials.getRawValue().password});
     if(data.error){
-      console.log('ERROR UPDATING PASSWORD',data.error.message)
+      console.log('ERROR UPDATING PASSWORD',data.error.message);
+      this.requestErrorHandler(data.error);
     }else{
+      this.updateSuccess = true;
+
       console.log('Password successfully updated')
     }
+    this.modalService.open(modal);
+  }
+
+  requestErrorHandler(error: AuthError) {
+    console.log(error.message);
+    this.requestErrorMessage = '';
+
+    if (error.status == 429) {
+      this.requestErrorMessage = 'Too many requests! ';
+    }
+    this.updateSuccess = false;
+    this.requestErrorMessage += error.message;
   }
 }
